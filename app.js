7b14c15b1870411928b515bcd44895c2d7d24159
Tanya <tanyadiit7@gmail.com>
2022-11-04 21:10:33 +0530

    made pop up

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
@@ -18,51 +18,51 @@ const Write = require("./models/write")
 const EventGallery = require("./models/eventGallery")
 const Event = require("./models/event")
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     res.render("home")
 });
 
-app.get("/writing", function(req,res){
+app.get("/writing", function (req, res) {
     Write.find({}, function (err, posts) {
         if (!err) res.render("writing-gallery", { posts: posts });
         else console.log(err);
     });
 });
-app.get("/art", function(req,res){
+app.get("/art", function (req, res) {
     Art.find({}, function (err, arts) {
         if (!err) res.render("art-gallery", { arts: arts });
         else console.log(err);
     });
 });
-app.get("/event", function(req,res){
+app.get("/event", function (req, res) {
     EventGallery.find({}, function (err, events) {
         if (!err) res.render("event-gallery", { events: events });
         else console.log(err);
     });
 });
 
-app.get("/events", function(req,res){
+app.get("/events", function (req, res) {
     Event.find({}, function (err, events) {
         if (!err) res.render("events", { events: events });
         else console.log(err);
     });
 });
 
-app.get("/team", function(req,res){
+app.get("/team", function (req, res) {
     res.render("team")
 });
 
-app.get("/contact", function(req,res){
+app.get("/contact", function (req, res) {
     res.render("contact")
 });
 
-app.get("/calendar", function(req,res){
+app.get("/calendar", function (req, res) {
     res.render("calendar")
 });
 
 app.post("/contact", contact.form);
 // const PORT = process.env.PORT || 3001
 const PORT = 3001
-app.listen(process.env.PORT || PORT, function(){
+app.listen(process.env.PORT || PORT, function () {
     console.log(`server stated on port ${PORT}`)
 })
