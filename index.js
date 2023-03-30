a46af5a3a1fa9712a1246df7bd7527adbd4313a2
SOCH-IGDTUW <103376360+SOCH-IGDTUW@users.noreply.github.com>
2023-03-30 11:53:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const contact = require('./routes/contact');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
