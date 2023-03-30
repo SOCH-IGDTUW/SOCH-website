ec1dbe78f8ec18304afde2e7a84223bdd3d96d28
SOCH-IGDTUW <103376360+SOCH-IGDTUW@users.noreply.github.com>
2023-03-30 11:56:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const path = require('path');
 const mongoose = require('mongoose');
 const contact = require('./routes/contact');
 
