394ae89f5e67559372c878878c1aef169e01ac0d
Tanya <tanyadiit7@gmail.com>
2023-04-02 18:58:52 +0530

    updated calender

--- public/JS/calendar_events.js
+++ public/JS/calendar_events.js
@@ -3,15 +3,6 @@ $(document).ready(function () {
     theme: "Orange Coral",
     calendarEvents: [
       {
-        id: "event1", // Event's ID (required)
-        name: "New Year", // Event name (required)
-        date: "January/1/2020", // Event date (required)
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-        type: "holiday", // Event type (required)
-        everyYear: true, // Same event every year (optional)
-      },
-      {
         name: "Writing Workshop",
         badge: "05/29", // Event badge (optional)
         date: ["May/29/2022", "May/29/2022"], // Date range
@@ -32,6 +23,142 @@ $(document).ready(function () {
         badge: "Lodhi Garden", // Event badge (optional)
         date: ["June/12/2022", "June/12/2022"], // Date range
         description: "Registration Fee: ₹50", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Valedictory",
+        badge: "Seminar Hall", // Event badge (optional)
+        date: ["April/2/2023", "April/2/2023"], // Date range
+        description: "Valedictory Ceremony of Batch 2021-22", // Event description (optional)
+        type: "event",
+        color: "#BB8193", // Event custom color (optional)
+      },
+      {
+        name: "Sketch Walk",
+        badge: "Garden of Five Senses", // Event badge (optional)
+        date: ["March/19/2023", "March/19/2023"], // Date range
+        description: "Plein Air Art Competition, Registration Fee: ₹70", // Event description (optional)
+        type: "event",
+        color: "#717873", // Event custom color (optional)
+      },
+      {
+        name: "Face Painting",
+        badge: "IGDTUW Ground", // Event badge (optional)
+        date: ["February/13/2023", "February/13/2023"], // Date range
+        description: "Face Painting in Synergy, Sports Fest of IGDTUW ", // Event description (optional)
+        type: "event",
+        color: "#59577D", // Event custom color (optional)
+      },
+      {
+        name: "Steller'23",
+        badge: "Google Meet", // Event badge (optional)
+        date: ["February/13/2023", "February/13/2023"], // Date range
+        description: "Open Mic by Tape a Tale", // Event description (optional)
+        type: "event",
+        color: "#514A7E", // Event custom color (optional)
+      },
+      {
+        name: "Quilling Workshop",
+        badge: "Seminar Hall", // Event badge (optional)
+        date: ["February/3/2023", "February/3/2023"], // Date range
+        description: " Learn the fine art of Quilling , Registration Fee: ₹70", // Event description (optional)
+        type: "event",
+        color: "#514A7E", // Event custom color (optional)
+      },
+      {
+        name: "Rewrite the End",
+        badge: "Seminar Hall", // Event badge (optional)
+        date: ["January/31/2023", "January/31/2023"], // Date range
+        description: " Tell us your ending, Registration Fee: ₹30", // Event description (optional)
+        type: "event",
+        color: "#514A7E", // Event custom color (optional)
+      },
+      {
+        name: "Kargil Divas",
+        badge: "IGDTUW", // Event badge (optional)
+        date: ["July/22/2022", "July/22/2022"], // Date range
+        description: "Show your Creativity", // Event description (optional)
+        type: "event",
+        color: "#514A7E", // Event custom color (optional)
+      },
+      {
+        name: "Doodle Series",
+        badge: "Social Media", // Event badge (optional)
+        date: ["June/8/2022", "July/3/2022"], // Date range
+        description: "Best Doodle Art of All", // Event description (optional)
+        type: "event",
+        color: "#514A7E", // Event custom color (optional)
+      },
+      {
+        name: "Quoteify",
+        badge: "Near Krishna Hostel", // Event badge (optional)
+        date: ["October/14/2022", "October/14/2022"], // Date range
+        description: "Write your Hearts Out", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Pop Art",
+        badge: "Taarangana 2022", // Event badge (optional)
+        date: ["April/8/2022", "April/8/2022"], // Date range
+        description: "Pop Culture and Mass Media", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Witchcraft",
+        badge: "Online", // Event badge (optional)
+        date: ["November/1/2022", "November/04/2022"], // Date range
+        description: "Microtales and Bodypaint", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Charcoal Workshop",
+        badge: "Online", // Event badge (optional)
+        date: ["February/13/2022", "February/13/2022"], // Date range
+        description: "Basic Sketching & Shading, Registration Fee: ₹30", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Writing Workshop",
+        badge: "Online", // Event badge (optional)
+        date: ["May/29/2022", "May/29/2022"], // Date range
+        description: "Writing Workshop For Beginners, Registration Fee: ₹20", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Monochrome Challenge",
+        badge: "Online", // Event badge (optional)
+        date: ["March/15/2022", "March/15/2022"], // Date range
+        description: " Monochromatic Painting Challenge", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Formidable Anecdote",
+        badge: "Online", // Event badge (optional)
+        date: ["February/6/2022", "February/6/2022"], // Date range
+        description: "Anecdote writing competition", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Watercolor Workshop",
+        badge: "Online", // Event badge (optional)
+        date: ["October/2/2021", "October/2/2021"], // Date range
+        description: "Beginner's watercolor workshop", // Event description (optional)
+        type: "event",
+        color: "#63d867", // Event custom color (optional)
+      },
+      {
+        name: "Mandala 2.0",
+        badge: "Google Meet", // Event badge (optional)
+        date: ["October/3/2021", "October/3/2021"], // Date range
+        description: " Intermediate Mandala Workshop ", // Event description (optional)
         type: "event",
         color: "#63d867", // Event custom color (optional)
       },
